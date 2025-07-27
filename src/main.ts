7566b883dd1bac810cd83f5f8ef696ad8adf1827
Onebluesky882 <onebluesky882@outlook.com>
2025-07-27 17:57:24 +0700

    open enablesCors

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,14 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  // app.enableCors({
-  //   origin: [
-  //     'http://localhost:5173',
-  //     'https://menu-x-five.vercel.app/',
-  //     'http://localhost:3001',
-  //   ],
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://menu-x-five.vercel.app/',
+      'http://localhost:3001',
+    ],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
